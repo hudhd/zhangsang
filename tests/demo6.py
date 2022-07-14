7cb893de4f669777eefd6059ef33f0b674ff77f8
34396 <3439605818@qq.com>
2022-07-14 10:22:05 +0800

    714

---
+++ tests/demo6.py
@@ -0,0 +1,12 @@
+p1 = 'hello world'
+print(p1)
+p2 = '你好世界'
+print(p2)
+p3 = '他说：“你好”'
+print(p3)
+p4 = '他说：\'你好\''
+print(p4)
+p5 = '他说：“你\n好”'
+print(p5)
+str1 = '大家好,我叫：黑鱼拜月'
+print(str1[len(str1)-4:])
